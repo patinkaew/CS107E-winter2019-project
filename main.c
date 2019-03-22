2550b617e5983e5071e534ad000fe1f5a3c67507
pinkaew <pinkaew@stanford.edu>
2019-03-21 22:59:19 -0700

    main testing water level

--- main.c
+++ main.c
@@ -1,36 +1,19 @@
-#include "printf.h"
+#include "modules/waterlevel.h"
+#include "gpio.h"
 #include "uart.h"
-#include "gl.h"
-#include "modules/plot.h"
-#include "modules/dataset.h"
-#include "malloc.h"
-#include <stddef.h>
-#include <stdint.h>
-#include <stdbool.h>
+#include "timer.h"
+#include "printf.h"
 
 void main(void){
+    gpio_init();
     uart_init();
-    gl_init(1024, 768, GL_DOUBLEBUFFER);
-    plot_info_t plot_info = {{8, 8}, {1016, 760}, true, {512, 384}, true, 1, "", false, true, "", false, 16, true, "", false, 16};
-    plot_init(plot_info);
-    dataset_init();
-
-    for(int i = -10; i <= 10; i++){
-        data_point_t* p1 = malloc(sizeof(point_t));
-        p1 -> x = i;
-        p1 -> y = i;
-        dataset_add_data(1, (uintptr_t)p1);
-        data_point_t* p2 = malloc(sizeof(point_t));
-        p2 -> x = i;
-        p2 -> y = ((float)i * i)/10;
-        dataset_add_data(2, (uintptr_t)p2);
+    timer_init();
+    waterlevel_init(GPIO_PIN3, GPIO_PIN2);
+    waterlevel_calibrate_tank_height();
+    printf("tank_height: %d %\n", waterlevel_get_tank_height());
+    timer_delay(2);
+    while(1){
+        printf("level: %d percent: %d %\n", waterlevel_get_level(), waterlevel_get_percent());
+        timer_delay_ms(250);
     }
-    /*point_t* p = malloc(sizeof(point_t));
-    p -> x = 10;
-    p -> y = 10;
-    dataset_add_data(1, (uintptr_t)p);*/
-    plot_add_dataset(dataset_get_dataset(1), 1, GL_RED, GL_AMBER);
-    plot_add_dataset(dataset_get_dataset(2), 1, GL_GREEN, GL_AMBER);
-    plot_update_screen();
-    printf("Finish\04");
 }
