b7df82336604a77702c31716d8dc44c4702cda95
Arjun Karanam <akaranam@stanford.edu>
2019-03-22 03:52:55 -0700

    Hope this works

--- main.c
+++ main.c
@@ -3,17 +3,20 @@
 #include "uart.h"
 #include "timer.h"
 #include "printf.h"
+#include "modules/moisture.h"
 
 void main(void){
     gpio_init();
     uart_init();
     timer_init();
-    waterlevel_init(GPIO_PIN3, GPIO_PIN2);
-    waterlevel_calibrate_tank_height();
-    printf("tank_height: %d %\n", waterlevel_get_tank_height());
-    timer_delay(2);
-    while(1){
-        printf("level: %d percent: %d %\n", waterlevel_get_level(), waterlevel_get_percent());
-        timer_delay_ms(250);
-    }
+
+    readSoil();
+    // waterlevel_init(GPIO_PIN3, GPIO_PIN2);
+    // waterlevel_calibrate_tank_height();
+    // printf("tank_height: %d %\n", waterlevel_get_tank_height());
+    // timer_delay(2);
+    // while(1){
+    //     printf("level: %d percent: %d %\n", waterlevel_get_level(), waterlevel_get_percent());
+    //     timer_delay_ms(250);
+    //}
 }
